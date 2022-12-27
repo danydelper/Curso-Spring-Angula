1cdbee3fe74e7b50973ec20fb6a5f8824b8e167e
Delper <daniel@delper.com.br>
2022-12-27 11:36:31 -0300

    primeiro commit

---
+++ src/test/java/br/com/delper/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delper.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
