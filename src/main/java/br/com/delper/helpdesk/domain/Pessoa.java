6c89778a13656b533cec918b4bbe5c86fbcd5b25
Delper <daniel@delper.com.br>
2022-12-29 15:04:29 -0300

    Versão até aula 06 - Anotações JPA para classes de dominio

--- src/main/java/br/com/delper/helpdesk/domain/Pessoa.java
+++ src/main/java/br/com/delper/helpdesk/domain/Pessoa.java
@@ -1,22 +1,49 @@
 package br.com.delper.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.delper.helpdesk.domain.enums.Perfil;
 
-public abstract class Pessoa {
-    
+@Entity(name = "CAD_Pessoa")
+public abstract class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
     protected String  nome;
+
+    @Column(unique = true)
     protected String  cpf;
+
+    @Column(unique = true)
     protected String  email;
     protected String  senha;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "CAD_PERFIS")
     protected Set<Integer> perfis = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     protected LocalDate dataCriacao = LocalDate.now();
 
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    protected LocalDate lastUpDate = LocalDate.now();
 
     // TODO: X1-Colocar dataCriação e Data Atualização datetime
     
